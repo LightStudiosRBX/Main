03afbdb4f510e2a0bc9201bb83065d9c97019245
Alex <54591924+AlexTheDev07@users.noreply.github.com>
2024-05-30 18:56:22 +0200

    Add files via upload

---
+++ JavaScript/Main.js
@@ -0,0 +1,31 @@
+function YouTube() {
+    window.open("https://www.youtube.com/channel/UC9SOZOJFYETPDjXMMrc0Esw")
+}
+
+function TikTok() {
+    window.open("https://www.tiktok.com/@l1ghtstud1os")
+}
+
+function Discord() {
+    window.open("https://discord.gg/4CnKHgqWWW")
+}
+
+function BattleStrike() {
+    window.open("https://www.roblox.com/games/15019035613/")
+}
+
+function Desert() {
+    window.open("https://www.roblox.com/games/13341248714/")
+}
+
+function Obby() {
+    window.open("https://www.roblox.com/games/3522352095/")
+}
+
+function VR() {
+    window.open("https://www.roblox.com/games/15269300961/")
+}
+
+function Join() {
+    window.open("https://www.roblox.com/groups/3812713/LightStudios#!/about")
+}
